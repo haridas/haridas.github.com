501942a7befed3261fb6873ff18eec9384977474
Haridas <haridas.nss@gmail.com>
2012-07-29 22:49:56 +0530

    A simple installer for vim from my repo. new file:   vim_installer.sh

---
+++ drafts/vim_installer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Simple shell script to clone my Vim and vimrc files from Github and setup
+# it under your machine.
+
+#  Script functionality.
+# 
+# Provide option for ,
+# 1. Download all required files.
+# 2. Setup it under home directory.
+# 3. Option to rever the installation completely.
+# 4. Update the project.
+
+GIT_URL='git://github.com/haridas/Dotfiles.git'
+
+HOME_DIR=$HOME
