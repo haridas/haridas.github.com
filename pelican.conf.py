c2ae0aceb47a67c67d41e74b0ec4d76499cd1c95
Haridas N <haridas.nss@gmail.com>
2014-05-08 21:38:16 +0530

    Added new theme to the blog. pure-single.

--- pelican.conf.py
+++ pelican.conf.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*- #
 
-AUTHOR = u"haridas"
-SITENAME = u"haridas.in"
+AUTHOR = u"Haridas N"
+SITENAME = u"Haridas N"
 # Don't, change the current URL scheme.
-SITEURL = 'http://haridas.in'
+SITEURL = '.'
 TIMEZONE = 'Asia/Kolkata'
 DEFAULT_LANG = 'en'
 
@@ -12,15 +12,20 @@ DEFAULT_PAGINATION = 4
 DEFAULT_ORPHANS = 2
 
 # Theme settings.
-THEME = '/mnt/data/projects/github/haridas.github.com/src/templates/notmyidea'
+THEME = '/mnt/data/projects/github/haridas.github.com/src/templates/pure-single'
 THEME_STATIC_PATHS = ['static']
-CSS_FILE = 'main.css'
+#CSS_FILE = 'main.css'
+CSS_FILE = 'pure.css'
 
 # Ordering of content.
 REVERSE_ARCHIVE_ORDER = True
 
 # Other menu items
-MENUITEMS = [('Archives', '/archives.html')]
+MENUITEMS = [('Archives', '/archives.html'),
+             ('About', '/pages/about-me.html')]
+
+COVER_IMG_URL = "images/Photz057.jpg"
+PROFILE_IMG_URL = "images/Image022.jpg"
 
 ## External services and Social medias.
 
@@ -31,16 +36,14 @@ TWITTER_USERNAME = 'haridas_n'
 
 # Blogroll
 LINKS = (
-    ('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
-    ('Python.org', 'http://python.org'),
-         )
+)
 
 # Social widget
 SOCIAL = (
-        ('twitter', 'http://twitter.com/haridas_n'),
-        ('github', 'http://github.com/haridas'),
-        ('linkedin', 'http://in.linkedin.com/pub/haridas-n/19/95/825'),
-         )
+    ('twitter-square', 'http://twitter.com/haridas_n'),
+    ('github', 'http://github.com/haridas'),
+    ('linkedin', 'http://in.linkedin.com/pub/haridas-n/19/95/825'),
+)
 
 ARTICLE_EXCLUDES = ['templates', 'pages']
-PAGE_EXCLUDES = ['articles', 'drafts', 'pages']
+PAGE_EXCLUDES = ['articles', 'drafts']
