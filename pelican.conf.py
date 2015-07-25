47115711121529099719d13fcdf8f4b3b423ad5b
Haridas Narayanaswamy <haridas.nss@gmail.com>
2015-07-25 17:16:19 +0530

    Fix for the pelican configuration.

--- pelican.conf.py
+++ pelican.conf.py
@@ -4,8 +4,8 @@ import os
 AUTHOR = u"HN"
 SITENAME = u"HN"
 # Don't, change the current URL scheme.
-SITEURL = os.path.dirname(__file__)
-#SITEURL = 'http://haridas.in'
+#SITEURL = os.path.dirname(__file__)
+SITEURL = 'http://haridas.in'
 
 TIMEZONE = 'Asia/Kolkata'
 DEFAULT_LANG = 'en'
