ebd8e8cf29e9d8c7810a2ce66610181601cf2f5d
Haridas N <haridas.nss@gmail.com>
2020-07-11 11:39:39 +0530

    New Alias post

--- pelican.conf.py
+++ pelican.conf.py
@@ -3,22 +3,22 @@ import os
 
 AUTHOR = u"HN"
 SITENAME = u"HN"
-# Don't, change the current URL scheme.
-# SITEURL = 'http://localhost:8000'
+# Don"t, change the current URL scheme.
+# SITEURL = "http://localhost:8000"
 #
-SITEURL = 'https://haridas.in'
+SITEURL = "https://haridas.in"
 
-TIMEZONE = 'Asia/Kolkata'
-DEFAULT_LANG = 'en'
+TIMEZONE = "Asia/Kolkata"
+DEFAULT_LANG = "en"
 
 # Pagination settings.
 DEFAULT_PAGINATION = 4
 DEFAULT_ORPHANS = 2
 
 # Theme settings.
-THEME = os.path.join(os.path.dirname(__file__), 'src/templates/hn-theme')
-THEME_STATIC_PATHS = ['static']
-CSS_FILE = 'pure.css'
+THEME = os.path.join(os.path.dirname(__file__), "src/templates/hn-theme")
+THEME_STATIC_PATHS = ["static"]
+CSS_FILE = "pure.css"
 
 # Ipython template directory setting.
 IPYTHON_TEMPLATE_PATH = os.path.join(os.path.dirname(__file__),
@@ -29,22 +29,22 @@ REVERSE_ARCHIVE_ORDER = True
 #
 # Properly resolve the URLs for each articles. THere is no nesting all the
 # blog entries are kept on the top level itself.
-CATEGORY_SAVE_AS='category/{slug}.html'
-CATEGORY_URL='category/{slug}.html'
-TAG_URL='tag/{slug}.html'
-TAG_SAVE_AS='tag/{slug}.html'
-ARTICLE_URL='/{slug}.html'
+CATEGORY_SAVE_AS="category/{slug}.html"
+CATEGORY_URL="category/{slug}.html"
+TAG_URL="tag/{slug}.html"
+TAG_SAVE_AS="tag/{slug}.html"
+ARTICLE_URL="/{slug}.html"
 
 # Other menu items
 MENUITEMS = [
-    #('Archives', 'archives.html'),
-    ('Programming', 'category/programming.html'),
-    ('Data-Science', 'category/data-science.html'),
-    ('Security', 'tag/security.html'),
-    ('Devops', 'category/devops.html'),
-    ('Talks', 'pages/my-talks.html'),
-    ('About Me', 'pages/about-me.html'),
-    ('Resume', 'resume.pdf')
+    #("Archives", "archives.html"),
+    ("Programming", "category/programming.html"),
+    ("Data-Science", "category/data-science.html"),
+    ("Security", "tag/security.html"),
+    ("Devops", "category/devops.html"),
+    ("Talks", "pages/my-talks.html"),
+    ("About Me", "pages/about-me.html"),
+    ("Resume", "resume.pdf")
 ]
 
 # COVER_IMG_URL = "./images/Photz057.jpg"
@@ -52,17 +52,17 @@ PROFILE_IMG_URL = "images/profile_pic.jpg"
 
 # External services and Social medias.
 
-DISQUS_SITENAME = 'haridas'
-GITHUB_URL = 'https://github.com/haridas'
-GOOGLE_ANALYTICS = 'UA-23592173-1'
-TWITTER_USERNAME = 'haridas_n'
+DISQUS_SITENAME = "haridas"
+GITHUB_URL = "https://github.com/haridas"
+GOOGLE_ANALYTICS = "UA-23592173-1"
+TWITTER_USERNAME = "haridas_n"
 
-MARKUP = ('md', 'ipynb', 'rst', 'adoc')
+MARKUP = ("md", "ipynb", "rst", "adoc")
 # Ascii doc markup settings.
-ASCIIDOC_CMD = 'asciidoctor'
+ASCIIDOC_CMD = "asciidoctor"
 ASCIIDOC_OPTIONS = []
-ASCIIDOC_BACKEND = 'html5'
-PLUGIN_PATHS = (os.path.join(os.path.dirname(__file__), './pelican-plugins'),)
+ASCIIDOC_BACKEND = "html5"
+PLUGIN_PATHS = (os.path.join(os.path.dirname(__file__), "./pelican-plugins"),)
 # plugins.
 PLUGINS = [
     "pelican_gist",
@@ -77,17 +77,17 @@ LINKS = (
 
 # Social widget
 #
-# Make sure that the name of the images won't trigger addblock ;)
+# Make sure that the name of the images won"t trigger addblock ;)
 #
 # Provide OPTIONAL (wxh) tuple if you want to resize the logo.
 #
 # FORMAT :- ( Label, logo, link, (width, height))
 SOCIAL = (
-    ('Twitter', 'images/tt.svg', 'https://twitter.com/haridas_n', ('48', '48')),
-    ('Github', 'images/github.svg', 'https://github.com/haridas', ('45', '45')),
-    ('Linkedin', 'images/linkedin.svg',
-     'https://linkedin.com/in/haridasn', ('48', '48'))
+    ("Twitter", "images/tt.svg", "https://twitter.com/haridas_n",("48", "48")),
+    ("Github", "images/github.svg", "https://github.com/haridas", ("45", "45")),
+    ("Linkedin", "images/linkedin.svg",
+     "https://linkedin.com/in/haridasn", ("48", "48"))
 )
 
-ARTICLE_EXCLUDES = ['templates', 'pages', 'draft-articles']
-PAGE_EXCLUDES = ['articles', 'drafts']
+ARTICLE_EXCLUDES = ["templates", "pages", "draft-articles"]
+PAGE_EXCLUDES = ["articles", "drafts"]
